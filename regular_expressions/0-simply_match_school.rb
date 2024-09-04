1a18b99421eb054af8c9a03972a8fe0293a84f88
Millowise01 <l.carew@alustudent.com>
2024-09-04 14:01:39 +0000

    match school

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,20 @@
 #!/usr/bin/env ruby
-# checks a word
-puts ARGV[0].scan(/School/).join
+
+# Check if an argument is provided
+if ARGV.length != 1
+  puts "Usage: ruby match_school.rb <string>"
+  exit 1
+end
+
+# Assign the argument to a variable
+input = ARGV[0]
+
+# Define the regular expression
+regexp = /School/
+
+# Match the input string against the regular expression
+if input.match?(regexp)
+  puts "Match found: #{input}"
+else
+  puts "No match found."
+end
