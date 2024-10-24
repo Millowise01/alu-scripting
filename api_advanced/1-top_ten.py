d91f7b917650097131b180ba294b986a980bcb21
Millowise01 <l.carew@alustudent.com>
2024-10-24 11:41:35 +0000

    top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,58 +1,16 @@
 #!/usr/bin/python3
-"""
-Module that queries the Reddit API and prints the titles 
-of the first 10 hot posts for a given subreddit
-"""
+""" top_ten.py """
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    for a given subreddit.
-    
-    Args:
-        subreddit: string - the name of the subreddit to query
-        
-    Returns:
-        None - prints the titles or None if subreddit is invalid
-    """
-    # Reddit API endpoint for hot posts
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    
-    # Custom User-Agent to avoid too many requests error
-    headers = {
-        'User-Agent': 'Custom User Agent 1.0',
-        # Disable automatic redirect following
-        'Accept': 'application/json'
-    }
-    
-    # Parameters to limit the number of posts
-    params = {
-        'limit': 10
-    }
-    
-    try:
-        # Make GET request to Reddit API
-        response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            allow_redirects=False
-        )
-        
-        # Check if subreddit exists (status code 200)
-        if response.status_code == 200:
-            # Parse JSON response
-            posts = response.json().get('data', {}).get('children', [])
-            
-            # Print titles of first 10 posts
-            for post in posts:
-                print(post.get('data', {}).get('title'))
-        else:
-            # Invalid subreddit or other error
-            print(None)
-            
-    except Exception as e:
-        # Handle any errors that might occur
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
