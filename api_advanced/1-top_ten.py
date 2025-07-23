f6b8d3fac5a80019e36401bb0ec0e00d7a047c75
Leroy <l.carew@alustudent.com>
2025-07-23 16:00:19 +0200

    update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"DOCS""
+"""DOCS"""
 import requests
 
 
 def top_ten(subreddit):
-    "Docs""
+    """Docs"""
     reddit_url = "https://www.reddit.com/r/{}/hot.json" \
         .format(subreddit)
-    headers = {'User-agent': 'Mozilla/5.0'}
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(reddit_url, headers=headers)
 
     if response.status_code == 200:
